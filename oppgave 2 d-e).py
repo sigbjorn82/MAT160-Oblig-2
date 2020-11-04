bdeda221ff1077cab5441dc4dffdd34c4fce9cca
sigbjorn82 <fjelland@icloud.com>
2020-11-04 20:08:43 +0100

    Ett noget dyssfunksjonelt plot, men dog ett plot i oppgave 2 (d)

--- oppgave 2 d-e).py
+++ oppgave 2 d-e).py
@@ -1,9 +1,9 @@
 import numpy as np
 from numpy.linalg import inv
+from numpy.linalg import norm
 import pandas as pd
 import scipy.sparse as sp
-import matplotlib
-
+import matplotlib.pyplot as plt
 
 def backU(U,b,N):
     '''Takes inn triangular matrix U, vector b and dimention of matrix n
@@ -27,13 +27,19 @@ def jacobi(A, b, x, n):
     LU = L+U
     x_iter = []
     e_i = []
+    x_norm = []
+    e_norm = []
 
     for i in range(n):
         x = np.dot(inv(D), (b - np.dot(LU,x)))
         x_iter.append(x)
         e = abs(np.dot(A,x)-b)
         e_i.append(e)
-    return pd.DataFrame({'x Approximations jacobi': x_iter}), pd.DataFrame({'errors': e_i})
+
+        e_norm.append(norm(e))
+        x_norm.append(norm(x))
+
+    return pd.DataFrame({'x Approximations jacobi': x_iter}), pd.DataFrame({'errors': e_i}), pd.DataFrame({'error_norm': e_norm}), pd.DataFrame({'x_norm': x_norm})
 
 def gaus_seidel_brutalforce(A, b, x_0, n):
     D =  np.diag(np.diag(A))
@@ -41,33 +47,40 @@ def gaus_seidel_brutalforce(A, b, x_0, n):
     L = np.tril(A)
     x_iter = []
     e_i = []
+    x_norm = []
+    e_norm = []
     for i in range(n):
         x_new = np.dot(inv(L), (b - np.dot(U, x_0)))
         x_iter.append(x_new)
         x_0 = x_new
-
-
         e = abs(np.dot(A,x_new)-b)
+
         e_i.append(e)
+        e_norm.append(norm(e))
+        x_norm.append(norm(x_new))
+    return pd.DataFrame({'x Approximations gaus_seidel_Brutal_Force': x_iter}), pd.DataFrame({'errors': e_i}), pd.DataFrame({'error_norm': e_norm}), pd.DataFrame({'x_norm': x_norm})
 
-    return pd.DataFrame({'x Approximations gaus_seidel_Brutal_Force': x_iter}), pd.DataFrame({'errors': e_i})
-
+from matplotlib.scale import LogScale
 def gaus_seidel_backsub(A, b, x_0, n):
     D =  np.diag(np.diag(A))
     U = np.triu(A)
     L = np.tril(A)-D
     x_iter = []
     e_i = []
+    x_norm = []
+    e_norm = []
     for i in range(n):
 
         x_new = backU(U= U, b= (b-np.dot(L,x_0)), N= A.shape[0])
         x_iter.append(x_new)
         x_0 = x_new
-
         e = abs(np.dot(A,x_new)-b)
-        e_i.append(e)
 
-    return pd.DataFrame({'x Approximations gaus_seidel_BackSubst': x_iter}), pd.DataFrame({'errors': e_i})
+        e_i.append(e)
+        e_norm.append(norm(e))
+        x_norm.append(norm(x_new))
+
+    return pd.DataFrame({'x Approximations gaus_seidel_BackSubst': x_iter}), pd.DataFrame({'errors': e_i}), pd.DataFrame({'error_norm': e_norm}), pd.DataFrame({'x_norm': x_norm})
 
 
 
@@ -84,8 +97,41 @@ b_alpha = np.ones((1000, 1))
 
 x0 =np.zeros((1000, 1), dtype='float')
 
-df=gaus_seidel_brutalforce(A_alpha(Alpha_1), b_alpha, x0, n=10)[1]
+dfGS1 = gaus_seidel_brutalforce(A_alpha(Alpha_1), b_alpha, x0, n=40)[2].cumsum()
+dfGS2 = gaus_seidel_brutalforce(A_alpha(Alpha_2), b_alpha, x0, n=40)[2].cumsum()
+#dfGS3 = gaus_seidel_brutalforce(A_alpha(Alpha_3), b_alpha, x0, n=200)[2].cumsum()
+#dfGS4 = gaus_seidel_brutalforce(A_alpha(Alpha_4), b_alpha, x0, n=200)[2].cumsum()
 
-print(df)
+plt.figure('Gaus-Seidell')
+
+plt.subplot()
+plt.plot(dfGS1)
+plt.plot(dfGS2)
+#plt.plot(dfGS3)
+#plt.plot(dfGS4)
+plt.grid(True)
+plt.yscale('log')
+
+plt.show()
+
+
+#plot Jacobi
+dfJ1 = jacobi(A_alpha(Alpha_1), b_alpha, x0, n=40)[2].cumsum()
+dfJ2 = jacobi(A_alpha(Alpha_1), b_alpha, x0, n=40)[2].cumsum()
+#dfJ3 = jacobi(A_alpha(Alpha_1), b_alpha, x0, n=200)[2].cumsum()
+#dfJ4 = jacobi(A_alpha(Alpha_1), b_alpha, x0, n=200)[2].cumsum()
+
+plt.figure('jacobi')
+
+plt.subplot()
+plt.plot(dfJ1)
+plt.plot(dfJ2)
+#plt.plot(dfJ3)
+#plt.plot(dfJ4)
+
+plt.grid(True)
+plt.yscale('log')
+
+plt.show()
 
 
